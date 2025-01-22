1567a8d9076e704eb94aa4fd77cd7a4b1ba3b4a9
Andrei1914 <124397816+Andrei1914@users.noreply.github.com>
2025-01-22 22:45:48 +0300

    main.py

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import random
 
-try: #блок try-except для непредвиденных ошибок
-    while True: #цикл для ограничений по данным в соответсвии с заданием
+try:  # блок try-except для непредвиденных ошибок
+    while True:  # цикл для ограничений по данным в соответствии с заданием
         n = int(input('Input long line: '))
-        k = int(input('Input num : '))
+        k = int(input('Input num: '))
         if 1 <= k <= n <= 2 * 10**5:
             break
 
-    color_list = [random.choice(['B', 'W']) for _ in range(n)] #рандомная выборка цветов и занесение в список в соответствии длинны от пользователя
+    color_list = [random.choice(['B', 'W']) for _ in range(n)]  # рандомная выборка цветов и занесение в список в соответствии длины от пользователя
 
-    max_poles = 0 #переменная для хранения числа максимальной последовательности
-    poles = 0 #переменная для хранения числа текущей последовательности
+    # Для хранения количества 'B' окне
+    current_count = 0
+    for color in color_list[:k]:
+        if color == 'B':
+            current_count += 1
 
-    for color in color_list: #цикл который отбирает толькор "B" и вычиляет макситальную последовательность с помью сравнения  макисмальной и текущей последовательности
-        if color == "B":
-            poles += 1
-        else:
-            if poles >= max_poles:
-                max_poles =  poles
-            poles = 0
-    
+            
+    min_b_count = current_count  # Минималное количество 'B' в окне
+
+    for i in range(k, n):
+        # Убираем цвет, который выходит из окна
+        if color_list[i - k] == 'B':
+            current_count -= 1
+        # Добавляем цвет, который входит в окно
+        if color_list[i] == 'B':
+            current_count += 1
+        
+        # Обновляем минимальное количество 'B' в окне
+        min_b_count = min(min_b_count, current_count)
+
     print(color_list)
-    print(0 if max_poles >= k  else k - max_poles)
-except ValueError as e: #Вывод ошибки в терминал
+    print(min_b_count)  # Выводим минимальное количество 'B' в окнах длиной k
+
+except ValueError as e:  # Вывод ошибки в терминал
     print("Пожалуйста, введите число\n", e)
